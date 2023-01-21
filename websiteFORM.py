ebdae41a1f119a6664b708c9f4e440072741ec8b
ACTSP <108674672+actsp@users.noreply.github.com>
2023-01-20 22:57:07 -0300

    Update websiteFORM.py

--- websiteFORM.py
+++ websiteFORM.py
@@ -19,20 +19,22 @@ st.set_page_config(
 new=2
 url = 'https://docs.google.com/forms/d/e/1FAIpQLScQyVpejEuPYzqCzUR9gF7lnRbz4UNF6rl8AOV8KNc3o84V4g/formResponse?&submit=Submit?usp=pp_url&entry.884978944= - '
 
+caminhoCursos = requests.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vQMMr7n2xkeFdK0eg_izhe1NN1QfgaLZ9OpBHrTMqgJ7ybMheGVvfrHcENYdl_68rh5_GNanRVutN0p/pub?gid=202745307&single=true&output=csv')
+data = caminhoCursos.content
+df = pd.read_csv(BytesIO(data), index_col=0)
+df.columns = ['Descricao','SituacaoCurso','Responsavel', 'DataInicio','DataTermino','CargaHoraria','HoraSEG','HoraTER','HoraQUA','HoraQUI','HoraSEX','HoraSAB','HoraDOM','OBScurso']
 
 #st.write(df.head())
 st.title('Cadastre seu interesse em cursos - ACT SP ')
 st.subheader('Entraremos em contato com você em breve!')
 
-import streamlit as st
-
-st.markdown("""
-               <style>
-               div.stButton > button:first-child {
-                   background-color: rgb(204, 49, 49);
-               }
-               </style>""", unsafe_allow_html=True)
-
+EmBreve = df['SituacaoCurso']=='Em breve'
+df01 = df[EmBreve]
+option = st.multiselect('Selecione o(s) curso(s) desejado(s):',df01['Descricao']+ '( Início: ' + df01['DataInicio'] + ', término: ' + df01['DataTermino'] + ')' )
+if option:     
+    st.write('Você selecionou o(s) curso(s): ')
+    st.write(option)
+    url = url + '&entry.1816236222=' + str(option)
 
 txtNome = st.text_input("Digite seu nome completo 👇")
 if txtNome:
@@ -53,5 +55,4 @@ if txtFONE:
     #+'&entry.374343962='  + str(txtPIX)
     #+'&entry.1226253104=' + str(txtPGTO)    
 if st.button('Confirmar 👇'):
-    #webbrowser.open(url,new=new)
-
+    webbrowser.open(url,new=new)
